52f0aa0890b19ddd3dfb326af13d521a43744cd4
Benefactor01 <szabolcs.katona23@gmail.com>
2019-05-13 22:22:06 +0200

    ControllerOfLogin fully documented

--- src/main/java/MovieCenter/Controller/ControllerOfLogin.java
+++ src/main/java/MovieCenter/Controller/ControllerOfLogin.java
@@ -22,24 +22,47 @@ import javafx.util.Duration;
 
 import java.io.IOException;
 
+/**
+ * {@code Controller} of the {@code Login} view.
+ */
 public class ControllerOfLogin extends Application {
 
+    /**
+     * {@link Label} where the output of the login will be shown.
+     */
     @FXML private Label labelsignin;
+
+    /**
+     * {@link TextField} where the {@code user} can write his/her username.
+     */
     @FXML private TextField usernamesignin;
+
+    /**
+     * {@link PasswordField} where the {@code user} can write his/her password.
+     */
     @FXML private PasswordField passwordsignin;
 
+    /**
+     * These 2 variables are for the window to be able to dragged, as it is an undecorated window.
+     */
     private double xOffset = 0;
     private double yOffset = 0;
 
+    /**
+     * This is the name of the only stage in the application
+     * and it is set on public, so it can be handled from other Controllers too.
+     */
     public static Stage guiStage;
 
+    /**
+     * It handles the click on button SignUp and makes the new fxml to be able to dragged.
+     * Loads the {@code Register.fxml} file, and set the new scene to it.
+     */
     @FXML
     private void signin_SignUp(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("view/Register.fxml"));
         Parent root = loader.load();
         guiStage.setScene(new Scene(root));
-
-        //Az új ablakot (register.fxml) mozgathatóvá teszi
 
         guiStage.getScene().setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
@@ -57,12 +80,27 @@ public class ControllerOfLogin extends Application {
         });
     }
 
-    //Jobb felső X
+    /**
+     * Handles the button in the top right corner,
+     * so if you click on the red X the program will exit.
+     */
     @FXML
     private void signin_bezaras(ActionEvent event){
         System.exit(0);
     }
 
+    /**
+     * Handles the whole login process.
+     * Checks if all the requirements fill and if so, the application will let you login.
+     * You have to give the right {@code user}'s name and password.
+     * Also there is a 2,5 seconds delay when you successfully logged in
+     * so you have enough time to read the text on the label that says "Successfully logged in!"
+     *
+     * After login, you'll be directed to the {@code Belepve.fxml}
+     * and it is also set to be able to dragged.
+     * The {@link TextField} will be sent to the new scene, as it contains the username
+     * and that has to be shown on all of the scenes.
+     */
     @FXML
     private void signin_SendSignIn() {
 
@@ -103,8 +141,6 @@ public class ControllerOfLogin extends Application {
                 labelsignin.setStyle("-fx-font-weight: bold; -fx-font-size: 24;");
                 labelsignin.setText("Sikeres bejelentkezés!");
 
-                //Időzítő 2.5 másodpercig hogy látható legyen a bejelentkezés sikeres felirat!
-
                 Timeline timeline = new Timeline(new KeyFrame(
                         Duration.millis(2500),
                         ae -> {
@@ -116,15 +152,12 @@ public class ControllerOfLogin extends Application {
                                 e.printStackTrace();
                             }
 
-                            //Belépett felhasználó továbbküldése ControllerOfInside-nek
                             ControllerOfInside transfer1 = belep.getController();
                             transfer1.transferMessage(usernamesignin.getText());
 
                             guiStage.close();
                             guiStage.setScene(new Scene(root, 800, 600));
                             guiStage.show();
-
-
 
                             guiStage.getScene().setOnMousePressed(new EventHandler<MouseEvent>() {
                                 @Override
@@ -140,13 +173,17 @@ public class ControllerOfLogin extends Application {
                                     guiStage.setY(event.getScreenY() - yOffset);
                                 }
                             });
-                            // Main.getStage().setMaximized(true);
                         }));
                 timeline.play();
             }
         }
     }
 
+    /**
+     * The {@code start} method is the first method to be loaded when the application starts.
+     * It initializes the first scene, makes the windows undecorated, set its paramteres
+     * and makes it draggable.
+     */
     @Override
     public void start(Stage stage) throws Exception {
         guiStage = stage;
---
+++ target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"MovieCenter.Controller"},{"l":"MovieCenter.model"},{"l":"MovieCenter.model.tables"}]
